3e531509665174667cd9e24fa0eecd4c72fcb44b
David Lomotey <80100400+lomotey1@users.noreply.github.com>
2023-10-27 05:50:47 +0000

    first test, broken build

--- config/pico/config.cpp
+++ config/pico/config.cpp
@@ -17,7 +17,21 @@
 #include "modes/Melee20Button.hpp"
 #include "stdlib.hpp"
 
+// ssd1306
+
+#include ".pio/libdeps/pico/pico-ssd1306/example/BMSPA_font.h"
+#include ".pio/libdeps/pico/pico-ssd1306/example/acme_5_outlines_font.h"
+#include ".pio/libdeps/pico/pico-ssd1306/example/bubblesstandard_font.h"
+#include ".pio/libdeps/pico/pico-ssd1306/example/crackers_font.h"
+#include ".pio/libdeps/pico/pico-ssd1306/example/image.h"
+#include ".pio/libdeps/pico/pico-ssd1306/ssd1306.h"
+#include "hardware/i2c.h"
+#include "pico/stdlib.h"
+
 #include <pico/bootrom.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 CommunicationBackend **backends = nullptr;
 size_t backend_count;
@@ -147,20 +161,101 @@ void loop() {
 }
 
 /* Nunchuk code runs on the second core */
-NunchukInput *nunchuk = nullptr;
+// NunchukInput *nunchuk = nullptr;
+
+// ssd1306 on second core
+const uint8_t num_chars_per_disp[] = { 7, 7, 7, 5 };
+const uint8_t *fonts[4] = { acme_font, bubblesstandard_font, crackers_font, BMSPA_font };
+
+#define SLEEPTIME 25
 
 void setup1() {
-    while (backends == nullptr) {
-        tight_loop_contents();
-    }
+    // while (backends == nullptr) {
+    //     tight_loop_contents();
+    // }
 
     // Create Nunchuk input source.
-    nunchuk = new NunchukInput(Wire, pinout.nunchuk_detect, pinout.nunchuk_sda, pinout.nunchuk_scl);
+    // nunchuk = new NunchukInput(Wire, pinout.nunchuk_detect, pinout.nunchuk_sda,
+    // pinout.nunchuk_scl);
+
+    // Setup OLED
+    i2c_init(i2c0, 400000);
+    gpio_set_function(8, GPIO_FUNC_I2C);
+    gpio_set_function(9, GPIO_FUNC_I2C);
+    gpio_pull_up(8);
+    gpio_pull_up(9);
 }
 
 void loop1() {
-    if (backends != nullptr) {
-        nunchuk->UpdateInputs(backends[0]->GetInputs());
-        busy_wait_us(50);
+    // if (backends != nullptr) {
+    //     nunchuk->UpdateInputs(backends[0]->GetInputs());
+    //     busy_wait_us(50);
+    // }
+
+    // animation method
+    const char *words[] = { "SSD1306", "DISPLAY", "DRIVER" };
+
+    ssd1306_t disp;
+    disp.external_vcc = false;
+    ssd1306_init(&disp, 128, 64, 0x3C, i2c0);
+    ssd1306_clear(&disp);
+
+    printf("ANIMATION!\n");
+
+    char buf[8];
+
+    for (;;) {
+        for (int y = 0; y < 31; ++y) {
+            ssd1306_draw_line(&disp, 0, y, 127, y);
+            ssd1306_show(&disp);
+            sleep_ms(SLEEPTIME);
+            ssd1306_clear(&disp);
+        }
+
+        for (int y = 0, i = 1; y >= 0; y += i) {
+            ssd1306_draw_line(&disp, 0, 31 - y, 127, 31 + y);
+            ssd1306_draw_line(&disp, 0, 31 + y, 127, 31 - y);
+            ssd1306_show(&disp);
+            sleep_ms(SLEEPTIME);
+            ssd1306_clear(&disp);
+            if (y == 32)
+                i = -1;
+        }
+
+        for (int i = 0; i < sizeof(words) / sizeof(char *); ++i) {
+            ssd1306_draw_string(&disp, 8, 24, 2, words[i]);
+            ssd1306_show(&disp);
+            sleep_ms(800);
+            ssd1306_clear(&disp);
+        }
+
+        for (int y = 31; y < 63; ++y) {
+            ssd1306_draw_line(&disp, 0, y, 127, y);
+            ssd1306_show(&disp);
+            sleep_ms(SLEEPTIME);
+            ssd1306_clear(&disp);
+        }
+
+        for (size_t font_i = 0; font_i < sizeof(fonts) / sizeof(fonts[0]); ++font_i) {
+            uint8_t c = 32;
+            while (c <= 126) {
+                uint8_t i = 0;
+                for (; i < num_chars_per_disp[font_i]; ++i) {
+                    if (c > 126)
+                        break;
+                    buf[i] = c++;
+                }
+                buf[i] = 0;
+
+                ssd1306_draw_string_with_font(&disp, 8, 24, 2, fonts[font_i], buf);
+                ssd1306_show(&disp);
+                sleep_ms(800);
+                ssd1306_clear(&disp);
+            }
+        }
+
+        // ssd1306_bmp_show_image(&disp, image_data, image_size);
+        // ssd1306_show(&disp);
+        // sleep_ms(2000);
     }
 }
